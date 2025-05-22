59bdcd81bcb09f1221d46f4771700a825909eebb
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2025-05-22 13:07:17 +0700

    Add: default task panels

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:qualita/utils/display_dialog.dart';
 import 'package:qualita/view/common/common_layout.dart';
 import 'package:qualita/view/home/add_project_form.dart';
 import 'package:qualita/view/home/project_select.dart';
+import 'package:qualita/view/home/task_panel.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,11 +15,26 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            SizedBox(child: ProjectSelect()),
-            IconButton(
-              onPressed: () => displayDialog(context, [AddProjectForm()]),
-              icon: Icon(Icons.add_circle_outline),
-              selectedIcon: Icon(Icons.add_circle),
+            Row(
+              children: [
+                Expanded(child: ProjectSelect()),
+                SizedBox(width: 16),
+                IconButton(
+                  onPressed: () => displayDialog(context, [AddProjectForm()]),
+                  icon: Icon(Icons.add_circle_outline),
+                  selectedIcon: Icon(Icons.add_circle),
+                ),
+              ],
+            ),
+            SizedBox(height: 32),
+            Row(
+              children: [
+                TaskPanel(name: 'To-Do'),
+                SizedBox(width: 16),
+                TaskPanel(name: 'Doing'),
+                SizedBox(width: 16),
+                TaskPanel(name: 'Done'),
+              ],
             ),
           ],
         ),
---
+++ lib/view/home/task_panel.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TaskPanel extends StatelessWidget {
+  final String name;
+
+  const TaskPanel({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.grey[300],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                name,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Icon(Icons.add),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
