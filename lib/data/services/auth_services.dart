c0cf0e792965cb09fedc09d09c11604d0a16d125
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2025-05-26 20:01:23 +0700

    Migration: to Supabase Auth

--- lib/data/services/auth_services.dart
+++ lib/data/services/auth_services.dart
@@ -1,25 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:qualita/data/models/user_model.dart';
-import 'package:qualita/data/services/user_services.dart';
+import 'package:qualita/global_keys.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthServices {
-  final _auth = FirebaseAuth.instance;
-  final _services = UserServices();
+  final _auth = supabase.auth;
 
   Future<void> signup(String email, String username, String password) async {
     try {
-      UserCredential cred = await _auth.createUserWithEmailAndPassword(
+      AuthResponse response = await _auth.signUp(
         email: email.trim(),
         password: password,
+        data: {'username': username},
       );
-      User? firebaseUser = cred.user;
 
-      if (firebaseUser != null) {
-        await _services.insert(
-          UserModel(id: firebaseUser.uid, username: username, email: email),
-        );
-      } else {
-        throw Exception('Unable to create user');
+      if (response.user == null) {
+        throw AuthException('Cannot create user');
       }
     } catch (e) {
       throw Exception('Unable to create user: ${e.toString()}');
@@ -28,14 +22,12 @@ class AuthServices {
 
   Future<void> signin(String email, String password) async {
     try {
-      UserCredential cred = await _auth.signInWithEmailAndPassword(
+      AuthResponse response = await _auth.signInWithPassword(
         email: email.trim(),
         password: password,
       );
-      User? firebaseUser = cred.user;
-
-      if (firebaseUser == null) {
-        throw FirebaseAuthException(code: 'user-not-found');
+      if (response.user == null) {
+        throw AuthException('User not found');
       }
     } catch (e) {
       throw Exception('Unable to login: ${e.toString()}');
@@ -43,14 +35,14 @@ class AuthServices {
   }
 
   Future<void> resetPassword(String email) async {
-    await _auth.sendPasswordResetEmail(email: email);
+    await _auth.resetPasswordForEmail(email);
   }
 
   User? isLoggedIn() {
     return _auth.currentUser;
   }
 
-  Stream<User?> onChange() {
-    return _auth.authStateChanges();
+  Stream<AuthState> onChange() {
+    return _auth.onAuthStateChange;
   }
 }
