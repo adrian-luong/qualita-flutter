c620e1bd9982890eee4526368902da17c7227fcb
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2025-07-15 12:56:05 +0700

    Edit: apply bottom tab bar for project settings and view

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qualita/data/providers/home_provider.dart';
 import 'package:qualita/view/common/common_layout.dart';
 import 'package:qualita/view/home/search_area.dart';
 import 'package:qualita/view/home/steps/step_area.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<StatefulWidget> createState() => _PageState();
+}
+
+class _PageState extends State<HomePage> {
+  int _selectedTab = 0;
+  final actualBodies = const [
+    StepArea(),
+    Center(child: Text('Project settings')),
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    final scheme = ColorScheme.of(context);
+    final provider = Provider.of<HomeProvider>(context);
+    var tabs = [
+      BottomNavigationBarItem(icon: Icon(Icons.view_kanban), label: 'Kanban'),
+      BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+    ];
+
     return CommonLayout(
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
@@ -16,10 +36,22 @@ class HomePage extends StatelessWidget {
             SizedBox(height: 16),
             SearchArea(),
             SizedBox(height: 16),
-            StepArea(),
+            actualBodies.elementAt(_selectedTab),
           ],
         ),
       ),
+      bottomNavBar:
+          provider.selectedProject != null
+              ? BottomNavigationBar(
+                showSelectedLabels: true,
+                showUnselectedLabels: true,
+                type: BottomNavigationBarType.fixed,
+                items: tabs,
+                currentIndex: _selectedTab,
+                selectedItemColor: scheme.primary,
+                onTap: (index) => setState(() => _selectedTab = index),
+              )
+              : null,
     );
   }
 }
